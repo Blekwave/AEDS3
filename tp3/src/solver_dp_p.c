bf09991b8f4bf5d082a6b4ef951c6526ae806419
Victor Diniz <blek.net@gmail.com>
2015-07-01 10:28:24 -0300

    Versão final (sem comentários)

--- tp3/src/solver_dp_p.c
+++ tp3/src/solver_dp_p.c
@@ -16,7 +16,6 @@ struct args_next_state {
 
 void *processNextState(void *args){
     struct args_next_state *a = args;
-    fprintf(stderr, "Alive! [%d, %d[ in %d\n", a->begin, a->end, a->item);
     int i;
     for (i = a->begin; i < a->end; i++){
         if (a->prev[i]){
@@ -34,7 +33,7 @@ int findBestSolution(int *seq, int seq_len, int init_val, int limit_val){
     pthread_attr_t attr;
     pthread_attr_init(&attr);
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-    
+
     bool *prev = calloc(sizeof(bool), limit_val + 1);
     bool *cur = calloc(sizeof(bool), limit_val + 1);
     prev[init_val] = true;
@@ -52,12 +51,12 @@ int findBestSolution(int *seq, int seq_len, int init_val, int limit_val){
     for (i = 0; i < seq_len; i++){
         for (j = 0; j < NUM_THREADS; j++){
             args[j].item = seq[i];
-            args[j].prev = prev[i];
-            args[j].cur = cur[i];
+            args[j].prev = prev;
+            args[j].cur = cur;
             pthread_create(&threads[j], &attr, processNextState, &args[j]);
         }
         for (j = 0; j < NUM_THREADS; j++){
-            pthread_join(threads[i], NULL);
+            pthread_join(threads[j], NULL);
         }
         bool *swap = cur;
         cur = prev;
@@ -66,7 +65,7 @@ int findBestSolution(int *seq, int seq_len, int init_val, int limit_val){
             cur[j] = false;
     }
     for (i = limit_val; i >= 0 && !prev[i]; i--);
-    
+
     pthread_attr_destroy(&attr);
     return i;
 }
